05f1f60505b254b45db6751aefc47f50da26d18b
vika <gimnast-96@mail.ru>
2022-03-19 16:05:37 +0300

    Dima

--- app/src/main/java/com/example/sleepapp/MainActivity.java
+++ app/src/main/java/com/example/sleepapp/MainActivity.java
@@ -102,9 +102,6 @@ public class MainActivity extends AppCompatActivity {
     private Button btnLoadData;
     private TextView result_info;
 
-    // Array of strings that's used to display on screen
-    String[] mobileArray = {"Android","IPhone","WindowsMobile","Blackberry",
-            "WebOS","Ubuntu","Windows7","Max OS X"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -315,9 +312,6 @@ public class MainActivity extends AppCompatActivity {
                 System.out.println("Session created");
                 sendEmail(session, toEmail,"SSLEmail Testing Subject", "SSLEmail Testing Body");
 
-                sendAttachmentEmail(session, toEmail,"SSLEmail Testing Subject with Attachment", "SSLEmail Testing Body with Attachment");
-
-
                     /*// проверяем доступность SD
                     if (!Environment.getExternalStorageState().equals(
                             Environment.MEDIA_MOUNTED)) {
@@ -445,55 +439,6 @@ public class MainActivity extends AppCompatActivity {
             System.out.println("EMail Sent Successfully!!");
         }
         catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-    public static void sendAttachmentEmail(Session session, String toEmail, String subject, String body){
-        try{
-            MimeMessage msg = new MimeMessage(session);
-            msg.addHeader("Content-type", "text/HTML; charset=UTF-8");
-            msg.addHeader("format", "flowed");
-            msg.addHeader("Content-Transfer-Encoding", "8bit");
-
-            msg.setFrom(new InternetAddress("no_reply@example.com", "NoReply-JD"));
-
-            msg.setReplyTo(InternetAddress.parse("gimnast-96@mail.ru", false));
-
-            msg.setSubject(subject, "UTF-8");
-
-            msg.setSentDate(new Date());
-
-            msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(toEmail, false));
-
-            // Create the message body part
-            BodyPart messageBodyPart = new MimeBodyPart();
-
-            // Fill the message
-            messageBodyPart.setText(body);
-
-            // Create a multipart message for attachment
-            Multipart multipart = new MimeMultipart();
-
-            // Set text message part
-            multipart.addBodyPart(messageBodyPart);
-
-            // Second part is attachment
-            messageBodyPart = new MimeBodyPart();
-            String filename = "abc.txt";
-            DataSource source = new FileDataSource(filename);
-            messageBodyPart.setDataHandler(new DataHandler(source));
-            messageBodyPart.setFileName(filename);
-            multipart.addBodyPart(messageBodyPart);
-
-            // Send the complete message parts
-            msg.setContent(multipart);
-
-            // Send message
-            Transport.send(msg);
-            System.out.println("EMail Sent Successfully with attachment!!");
-        }catch (MessagingException e) {
-            e.printStackTrace();
-        } catch (UnsupportedEncodingException e) {
             e.printStackTrace();
         }
     }
