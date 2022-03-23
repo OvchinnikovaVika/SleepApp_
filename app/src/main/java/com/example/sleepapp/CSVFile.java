ad98fe48c1e592d1f851faba18c18cf0dace8930
vika <gimnast-96@mail.ru>
2022-03-23 10:12:56 +0300

    Вика: Создан новый класс для работы с CSV-файлом, туда перемещена часть кода

--- app/src/main/java/com/example/sleepapp/CSVFile.java
+++ app/src/main/java/com/example/sleepapp/CSVFile.java
@@ -1,43 +1,112 @@
 package com.example.sleepapp;
 
-import android.util.Log;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Locale;
+
+import javax.xml.datatype.Duration;
 
 public class CSVFile {
-    InputStream inputStream;
 
-    public CSVFile(InputStream inputStream){
-        this.inputStream = inputStream;
+    public CSVFile() {
     }
 
-    public List read(){
-        List resultList = new ArrayList();
-        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-        try {
-            String csvLine;
-            while ((csvLine = reader.readLine()) != null) {
-                String[] row = csvLine.split(",");
-                resultList.add(row);
-                Log.d("VariableTag", row[0].toString());
+    public List<DataModel> getDataModelFromListSleepResult(List<List<String>> listSleepResult,Date dateUserStart, Date dateUserEnd) {
+
+        // + Vika 22.03.22
+        // Перевести в dataModels, разбить на подстроки
+
+        List<DataModel> dataModels = new ArrayList<>();
+
+        for (List<String> row : listSleepResult)
+        {
+            DataModel elementDataModel = new DataModel();
+
+            String[] strarray = row.toArray(new String[0]);
+
+            elementDataModel.setRecordCategory(strarray[0].substring(1, 4));
+            elementDataModel.setRecordSubCategory(strarray[0].split("\".+?\"")[1]);
+            elementDataModel.setStartDate(strarray[0].split("\".+?\"")[2]);
+
+            //getStringToDate
+            // String finishDateStr = strarray[0].split("\".+?\"")[3];
+            String finishDateStr = strarray[0].split("\".+?\"")[3]; // убрать тире и точки
+
+            if (finishDateStr.length()==18) { // при условии, что месяц записан 3 буквами
+                String finishDateStrSubstring1 = finishDateStr.substring(0,12);  // получаем строку формата:
+                String finishDateStrSubstring2 = finishDateStr.substring(13,18);
+
+                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("-", " ");
+//                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.\\s", ".");
+                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.", " ");
+
+                finishDateStr = finishDateStrSubstring1 + " " + finishDateStrSubstring2;
             }
-        }
-        catch (IOException ex) {
-            throw new RuntimeException("Error in reading CSV file: "+ex);
-        }
-        finally {
+            else {
+                String finishDateStrSubstring1 = finishDateStr.substring(0,13);  // получаем строку формата:
+                String finishDateStrSubstring2 = finishDateStr.substring(14,19);
+
+                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("-", " ");
+//                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.\\s", ".");
+                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.", " ");
+
+                finishDateStr = finishDateStrSubstring1 + " " + finishDateStrSubstring2;
+            }
+
+            Date startDate = new Date();
+            Date endDate = new Date();
             try {
-                inputStream.close();
+                endDate = new SimpleDateFormat("d MMM  yyyy HH:mm", Locale.getDefault()).parse(finishDateStr);
+
+            } catch (ParseException e) {
+                e.printStackTrace();
             }
-            catch (IOException e) {
-                throw new RuntimeException("Error while closing input stream: "+e);
+
+
+            if (endDate.before(dateUserEnd) && (startDate.after(dateUserStart))
+                    || endDate.after(dateUserStart) && (startDate.after(dateUserStart))){
+
             }
+                // если дата окончания из файла позже даты пользователя
+
+
+            elementDataModel.setFinishDate(endDate);
+            elementDataModel.setDetails(strarray[0].split("\".+?\"")[4]);
+
+            dataModels.add(elementDataModel);//
         }
-        return resultList;
+
+        // - Vika 22.03.22
+        // 3.2 Отбирать записи с категорией "Сон"
+        // и датой окончания больше даты окончания периода пользователя
+        //И ( датой начала меньше даты начала периода пользователя
+        //или датой начала больше даты начала периода пользователя)
+        return dataModels;
     }
+
+    // функция Сравнение дат
+    // + Вика 21.03
+    public static String compareDates(Date dateStart, Date dateEnd) {
+
+        // Сравнение дат
+        boolean isAfter = dateStart.after(dateEnd); //Проверяет, является ли эта дата после указанной даты.
+        // true если и только если момент, представленный этим объектом Date, строго позже момента, представленного dateEnd; false в противном случае.
+        boolean isBefore = dateStart.before(dateEnd);
+        // true если и только если момент времени, представленный этим объектом Date, строго раньше момента, представленного dateEnd; false в противном случае.
+
+        if (isAfter)
+            return "isAfter";
+        else if (isBefore)
+            return "isBefore";
+        else
+            return null;
+
+    }
+    // - Вика 21.03
+
+
 }
--- app/src/main/java/com/example/sleepapp/MainActivity.java
+++ app/src/main/java/com/example/sleepapp/MainActivity.java
@@ -70,7 +70,9 @@ import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Row;
+
 import com.example.sleepapp.DataModel;
+import com.example.sleepapp.CSVFile.*;
 
 
 import java.util.Properties;
@@ -165,19 +167,7 @@ public class MainActivity extends AppCompatActivity {
                         // Выделено в функцию
                         // Надо перевести строки в даты, понять какой нужен формат,
                         // формат - "16-мар.-2022 00:00" в файле, для сравнения 22.02.2022
-                        Date dateStart;
-                        Date dateEnd;
-//                        String dateStartStr = "22.02.2022";//fieldDateStart.getText().toString()
-//                        String dateEndStr = "23.02.2022";//fieldDateEnd.getText().toString()
 
-
-
-                        dateStart = getStringToDate(fieldDateStart.getText().toString());
-                        dateEnd = getStringToDate(fieldDateEnd.getText().toString());
-
-
-                        // Сравнение дат
-                        String resultCompare = compareDates(dateStart,dateEnd);
 
                         new GetFileData().execute(fileName); // Считать данные из файла *.csv
                         new LoadDataInTable().execute(fileTable); // Загрузить данные в файл *.xls
@@ -245,16 +235,15 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-
-
     // Ilya 21.03
-    // Получение листа с позициями сон из файла BabyRecords.csv
+    // Получение данных из файла пользователя BabyRecords.csv
+    // Формируется список строк listSleepResult
     public void getDataFromFile() {
 
         List<List<String>> listSleepResult = new ArrayList<>();
 
-        try (BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open("BabyRecords.csv")));
-             ) {
+        try (BufferedReader reader = new BufferedReader(
+                new InputStreamReader(getAssets().open("BabyRecords.csv")))) {
 
             String line;
             String sleep = "Сон"; //son - sleep Vika
@@ -263,89 +252,25 @@ public class MainActivity extends AppCompatActivity {
 
                 String flName = line.substring(1, 4);
 
-                if (flName.equals(sleep)) {//son - sleep Vika
+                if (flName.equals(sleep)) //son - sleep Vika
                     listSleepResult.add(Collections.singletonList(line));
-
-                }
-
             }
-
-
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        // Обработать файл "son.csv"
-//        List<List<String>> listResult = getFileToList("sleep.csv");
+        // Получить даты от пользователя, которые он ввёл
+        Date dateStart = getStringToDate(fieldDateStart.getText().toString());
+        Date dateEnd = getStringToDate(fieldDateEnd.getText().toString());
 
-        // + Vika 22.03.22
-        // Перевести в dataModels, разбить на подстроки
-
-        List<DataModel> dataModels = new ArrayList<>();
-
-
-
-        for (List<String> row : listSleepResult)
-        {
-            DataModel elementDataModel = new DataModel();
-
-            String[] strarray = row.toArray(new String[0]);
-
-            elementDataModel.setRecordCategory(strarray[0].substring(1, 4));
-            elementDataModel.setRecordSubCategory(strarray[0].split("\".+?\"")[1]);
-            elementDataModel.setStartDate(strarray[0].split("\".+?\"")[2]);
-
-            //getStringToDate
-           // String finishDateStr = strarray[0].split("\".+?\"")[3];
-            String finishDateStr = strarray[0].split("\".+?\"")[3]; // убрать тире и точки
-
-            if (finishDateStr.length()==18) { // при условии, что месяц записан 3 буквами
-                String finishDateStrSubstring1 = finishDateStr.substring(0,12);  // получаем строку формата:
-                String finishDateStrSubstring2 = finishDateStr.substring(13,18);
-
-                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("-", " ");
-//                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.\\s", ".");
-                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.", " ");
-
-                finishDateStr = finishDateStrSubstring1 + " " + finishDateStrSubstring2;
-            }
-            else {
-                String finishDateStrSubstring1 = finishDateStr.substring(0,13);  // получаем строку формата:
-                String finishDateStrSubstring2 = finishDateStr.substring(14,19);
-
-                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("-", " ");
-//                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.\\s", ".");
-                finishDateStrSubstring1 = finishDateStrSubstring1.replaceAll("\\.", " ");
-
-                finishDateStr = finishDateStrSubstring1 + " " + finishDateStrSubstring2;
-            }
-
-
-            //finishDateStr
-
-            try {
-                Date endDate = new SimpleDateFormat("d MMM  yyyy HH:mm",Locale.getDefault()).parse(finishDateStr);
-
-            } catch (ParseException e) {
-                e.printStackTrace();
-            }
-
-            // new SimpleDateFormat("d MMM yyyy HH:mm",Locale.getDefault()).parse("17 мар 2022 04:17")
-
-            // elementDataModel.setFinishDate(endDate);
-            elementDataModel.setDetails(strarray[0].split("\".+?\"")[4]);
-
-            dataModels.add(elementDataModel);//
-        }
-
-
-        // - Vika 22.03.22
-        // 3.2 Отбирать записи с категорией "Сон"
-        // и датой окончания больше даты окончания периода пользователя
-        //И ( датой начала меньше даты начала периода пользователя
-        //или датой начала больше даты начала периода пользователя)
+        // + Vika 23.03.22
+        // Появился новый класс CSVFile
+        CSVFile workWithCSV = new CSVFile();
+        List<DataModel> dataModels =
+                workWithCSV.getDataModelFromListSleepResult(listSleepResult, dateStart, dateEnd);
+        // - Vika 23.03.22
     }
 
     //функция для получения Листа листов из файла
@@ -398,25 +323,7 @@ public class MainActivity extends AppCompatActivity {
         Date date = new SimpleDateFormat("y-M-d H:m:s.S").parse("2017-9-11 13:1:28.9");
     }
 
-    // функция Сравнение дат
-    // + Вика 21.03
-    public static String compareDates(Date dateStart, Date dateEnd) {
 
-        // Сравнение дат
-        boolean isAfter = dateStart.after(dateEnd); //Проверяет, является ли эта дата после указанной даты.
-        // true если и только если момент, представленный этим объектом Date, строго позже момента, представленного dateEnd; false в противном случае.
-        boolean isBefore = dateStart.before(dateEnd);
-        // true если и только если момент времени, представленный этим объектом Date, строго раньше момента, представленного dateEnd; false в противном случае.
-
-        if (isAfter)
-            return "isAfter";
-        else if (isBefore)
-            return "isBefore";
-        else
-            return null;
-
-    }
-    // - Вика 21.03
 
     // + Вика 21.03
     // заполнение строки (rowNum) определенного листа (sheet)
